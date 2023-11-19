693f55a9f81f5d5930a96824f5bc3da34839e2c9
mohit <mohit@MacBook-Pro-2.local>
2023-11-19 21:36:02 +0530

    add path

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 ////////////////////////////REQUIREMENTS
 var express=require('express')
+var path = require("path")
 var parser=require('body-parser')
 var mongoose=require('mongoose')
 var methodOverride=require('method-override')
